4bbe35df51a7d534452cd00fe51b9f5ecba1e7df
msak-ai <kayvon.m.adderley@gmail.com>
2024-06-09 15:39:56 -0400

    Jun 9:2

--- app/layout.js
+++ app/layout.js
@@ -16,19 +16,17 @@ export default function RootLayout({ children }) {
         <title>CSU Physics</title>
       </head>
       <body className={inter.className}>
-        
+
         <script type="text/javascript">
-          var sc_project=13006815;
+          var sc_project=13006461;
           var sc_invisible=1;
-          var sc_security="af67b0b2";
+          var sc_security="3ab8d0c3";
         </script>
-        <script type="text/javascript"
-          src="https://www.statcounter.com/counter/counter.js" async></script>
-        <noscript><div class="statcounter"><a title="Web Analytics"
-          href="https://statcounter.com/" target="_blank"><img class="statcounter"
-            src="https://c.statcounter.com/13006815/0/af67b0b2/1/" alt="Web Analytics"
-            referrerPolicy="no-referrer-when-downgrade"/>
-        </a></div></noscript>
+        <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+        <noscript><div class="statcounter">
+          <a title="Web Analytics" href="https://statcounter.com/" target="_blank">
+            <img class="statcounter" src="https://c.statcounter.com/13006461/0/3ab8d0c3/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade" />
+          </a></div></noscript>
         {children}
       </body>
 
